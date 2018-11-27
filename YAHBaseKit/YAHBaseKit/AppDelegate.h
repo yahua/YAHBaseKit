6d354f5a86d6f216dc232ed031831200b027ef49
王时温 <yahua523@163.com>
2018-11-27 23:58:15 +0800

    添加基本工程

---
+++ YAHBaseKit/YAHBaseKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YAHBaseKit
+//
+//  Created by wangshiwen on 2018/11/27.
+//  Copyright © 2018 yahua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YAHBaseKit/YAHBaseKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAHBaseKit
+//
+//  Created by wangshiwen on 2018/11/27.
+//  Copyright © 2018 yahua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
