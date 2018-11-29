e6a3beae0fa9c21330be242628372b1625e41f75
王时温 <yahua523@163.com>
2018-11-29 23:45:54 +0800

    添加基本文件

---
+++ YAHBaseKit/YHCommon.h
@@ -0,0 +1,55 @@
+//
+//  YHCommon.h
+//  YHCommonDemo
+//
+//  Created by yahua on 16/3/3.
+//  Copyright © 2016年 wangsw. All rights reserved.
+//
+
+#ifndef YHCommon_h
+#define YHCommon_h
+
+//extobjc
+#import "EXTScope.h"
+
+//NSObject
+#import "NSObject+Block.h"
+
+//ToolUtils
+#import "Utility.h"
+
+//NSData
+#import "NSData+Base64.h"
+#import "NSData+MD5.h"
+
+//NSDate
+#import "NSDate+Utilities.h"
+
+//NSString
+#import "NSString+Base64_YAH.h"
+#import "NSString+Contains_YAH.h"
+#import "NSString+RegexCategory_YAH.h"
+#import "NSString+Size_YAH.h"
+#import "NSString+Hash_YAH.h"
+#import "NSString+Transfer_YAH.h"
+
+//NSStimer
+#import "NSTimer+Addition.h"
+#import "NSTimer+Blocks.h"
+
+//UIColor
+#import "UIColor+HEX.h"
+
+//UIImage
+#import "UIImage+Blur.h"
+#import "UIImage+Color.h"
+#import "UIImage+FileName.h"
+#import "UIImage+FX.h"
+#import "UIImage+Orientation.h"
+#import "UIImage+SuperCompress.h"
+
+//YAHKVOController
+#import "YAHKVOController.h"
+
+
+#endif /* YHCommon_h */
