2bc0786082c431b91f5e575a9b97df0cd464eaac
wangshiwen <yahua523@163.com>
2019-10-08 14:29:10 +0800

    yahlog调整

--- YAHBaseKit/extobjc/EXTScope.h
+++ YAHBaseKit/extobjc/EXTScope.h
@@ -110,7 +110,7 @@ block(__VA_ARGS__);\
 
 #ifdef DEBUG
 #define LRString [NSString stringWithFormat:@"%s", __FILE__].lastPathComponent
-#define YAHLog(...) printf("%s %s 第%d行: %s\n\n",[[[NSDate date] stringWithFormat:@"YYYY-MM-dd hh:mm:ss.SSS"] UTF8String], [LRString UTF8String] ,__LINE__, [[NSString stringWithFormat:__VA_ARGS__] UTF8String]);
+#define YAHLog(format, ...) NSLog(format, ## __VA_ARGS__)
 #else
 #   define YAHLog(...)
 #endif
